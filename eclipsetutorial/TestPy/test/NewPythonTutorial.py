e6b10fb118f45c68df670e5b9cd07d693cb58e82
user <user@FT-DESKTOP-019>
2016-07-22 17:25:01 +0530

    committing initial

---
+++ eclipsetutorial/TestPy/test/NewPythonTutorial.py
@@ -0,0 +1,8 @@
+from __init__ import *
+print(var1)
+print(var2)
+print(var3)
+tinydict = {'name': 'john','code':6734, 'dept': 'sales'}
+print (tinydict.keys())   # Prints all the keys
+print (tinydict.values()) # Prints all the values
+
---
+++ eclipsetutorial/TestPy/test/__init__.py
@@ -0,0 +1,3 @@
+var1="Testing __init__ Package"
+var2="Hello to PyDev"
+var3="Hope you enjoy the features of the python development environment in eclipse."

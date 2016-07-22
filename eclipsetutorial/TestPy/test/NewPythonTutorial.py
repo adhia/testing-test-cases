5a94eaa18b7e428920eb3333d8b1dab053dc0e9b
Adhithya <user@FT-DESKTOP-019>
2016-07-22 17:33:30 +0530

    test

--- eclipsetutorial/TestPy/test/NewPythonTutorial.py
+++ eclipsetutorial/TestPy/test/NewPythonTutorial.py
@@ -5,3 +5,18 @@ print(var3)
 tinydict = {'name': 'john','code':6734, 'dept': 'sales'}
 print (tinydict.keys())   # Prints all the keys
 print (tinydict.values()) # Prints all the values
+import unittest
+
+class TestCase(unittest.TestCase):
+
+    def setUp(self):
+        unittest.TestCase.setUp(self)
+
+    def tearDown(self):
+        unittest.TestCase.tearDown(self)
+
+    def testMet1(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()

14f25ff6277fc55337b65e451ade3c5b78b02be4
Adhithya Srinivasan <adhithyasrinivasan@gmail.com>
2016-09-01 18:36:32 +0530

    Update NewPythonTutorial.py

--- eclipsetutorial/TestPy/test/NewPythonTutorial.py
+++ eclipsetutorial/TestPy/test/NewPythonTutorial.py
@@ -2,6 +2,6 @@ from __init__ import *
 print(var1)
 print(var2)
 print(var3)
-tinydict = {'name': 'john','code':6734, 'dept': 'sales'}
+tinydict = {'name': 'john','code':6735, 'dept': 'sales'}
 print (tinydict.keys())   # Prints all the keys
 print (tinydict.values()) # Prints all the values

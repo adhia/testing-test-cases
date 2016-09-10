80db5dec37065eab4dac8a2a7151ae03c07d5fa9
Adhithya <user@FT-DESKTOP-019>
2016-09-10 15:48:56 +0530

    Selenium master

--- eclipsetutorial/TestPy/test/__init__.py
+++ eclipsetutorial/TestPy/test/__init__.py
@@ -1,3 +1,16 @@
-var1="Testing __init__ Package"
-var2="Hello to PyDev"
-var3="Hope you enjoy the features of the python development environment in eclipse."
+# var1="Testing __init__ Package"
+# var2="Hello to PyDev"
+# var3="Hope you enjoy the features of the python development environment in eclipse."
+
+import ConfigParser
+import io
+import openpyxl
+
+from fileinput import filename
+def readExcel():
+    print "a"
+
+def readConfig(filename):
+    config=ConfigParser.RawConfigParser()
+    config.read(filename)
+    return config
